5b0d4226de3343c43f09f30e14b0a08479fe7f66
Sarveshreg <sarveshregmi1@gmail.com>
2023-10-16 20:56:47 -0500

    block07

---
+++ Coursework/Unit1/block07/secureTheVault.js
@@ -0,0 +1,7 @@
+let numCow=50;
+let myString="hello world";
+// console.log(myString)
+// console.log("hello, "+ "disney land" )
+// let numUmg=10*3;
+// console.log(numUmg)
+alert(numCow+" " + myString)
